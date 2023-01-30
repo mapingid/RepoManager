ba85df73b85b8b9c85bb21f9b47830d241360b8d
Firman Ardiyansyah <firman.ardiyansyah@formulatrix.com>
2023-01-30 14:59:28 +0700

    Add Manage.cs; Add Repo.cs;

--- RepoManager/Program.cs
+++ RepoManager/Program.cs
@@ -6,10 +6,32 @@ using System.Threading.Tasks;
 
 namespace RepoManager
 {
+
   class Program
   {
     static void Main( string[] args )
     {
+      Manager Repository = new Manager();
+            
+      Repository.Register( "NameA", "Content A", Types.XML );
+      Repository.Register( "NameB", "Content B", Types.JSON );
+      Repository.Register( "NameC", "Content C", Types.XML );
+
+      Console.WriteLine( Repository.Retrieve( "NameB" ) );
+      Console.WriteLine( Repository.GetTypes( "NameB" ) );
+
+      Repository.Deregister( "NameB" );
+
+      Console.WriteLine( Repository.Retrieve( "NameB" ) );
+      Console.WriteLine( Repository.GetTypes( "NameB" ) );
+      Repository.Deregister( "NameB" );
+
+      Repository.Register( "NameA", "Content A", Types.JSON );
+      Repository.Register( "NameB", "Content B", Types.JSON );
+
+      Console.WriteLine( Repository.Retrieve( "NameB" ) );
+
+      Console.Read();
     }
   }
 }
